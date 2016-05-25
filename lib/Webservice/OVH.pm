d2475bb51c70d101709b98b2fe2dc36560267407
Patrick Jendral <pj@itnode.de>
2016-05-25 11:27:08 +0200

    Alternate constructor for json file

--- lib/Webservice/OVH.pm
+++ lib/Webservice/OVH.pm
@@ -11,9 +11,40 @@ use Webservice::OVH::Domain;
 use Webservice::OVH::Me;
 use Webservice::OVH::Order;
 
+use JSON;
+use File::Slurp qw(read_file);
+
 # Class variables
 
 # End - Class variables
+
+sub new_from_json {
+
+    my ( $class, $file_json ) = @_;
+    
+    my $json = read_file $file_json, {binmode => ':utf8'};
+    
+    my $Json = JSON->new->allow_nonref;
+    my $data = $Json->decode( $json );
+    
+    my @keys = qw{ application_key application_secret consumer_key };
+
+    if ( my @missing_parameters = grep { not $data->{$_} } @keys ) {
+
+        croak "Missing parameter: @missing_parameters";
+    }
+    
+    my $api_wrapper = OvhApi->new( 'type' => "https://eu.api.ovh.com/1.0", applicationKey => $data->{application_key}, applicationSecret => $data->{application_secret}, consumerKey => $data->{consumer_key} );
+    my $domain      = Webservice::OVH::Domain->_new($api_wrapper);
+    my $me          = Webservice::OVH::Me->_new($api_wrapper);
+    my $order       = Webservice::OVH::Order->_new($api_wrapper);
+    
+    OvhApi->setRequestTimeout( timeout => $data->{timeout} || 120 );
+
+    my $self = bless { _api_wrapper => $api_wrapper }, $class;
+    
+    return $self;
+}
 
 sub new {
 
@@ -72,11 +103,11 @@ sub order {
 }
 
 sub new_account {
-    
-    my ($self, $email, $firstname, $birthday) = @_;
-    
-    my $body =  { birthday => $birthday, firstname => $firstname, country => 'DE', email => $email, legalform => 'individual', ovhCompany => 'ovh', ovhSubsidiary => 'DE' };
-    
+
+    my ( $self, $email, $firstname, $birthday ) = @_;
+
+    my $body = { birthday => $birthday, firstname => $firstname, country => 'DE', email => $email, legalform => 'individual', ovhCompany => 'ovh', ovhSubsidiary => 'DE' };
+
     my $api = $self->{_api_wrapper};
     my $response = $api->rawCall( method => 'post', path => "/newAccount", noSignature => 0 );
     croak $response->error if $response->error;
