05c61d574938f2ae202dfa496b7a92319bbad3ae
Patrick Jendral <pj@itnode.de>
2016-05-25 11:04:29 +0200

    Helper module for Helper methods

---
+++ lib/Webservice/OVH/Helper.pm
@@ -0,0 +1,27 @@
+package Webservice::OVH::Helper;
+
+sub construct_filter {
+    
+    my ($class, %params) = @_;
+    
+    my @params = keys %params;
+    my @values = values %params;
+    my $filter = scalar @values ? '?' : "";
+    
+    foreach my $param ( @params ) {
+        
+        my $value = $params{$param};
+        next unless $value;
+        
+        if( $filter ne '?') {
+            
+            $filter.= '&';
+        }
+        $filter .= sprintf("%s=%s", $param, $value);
+    }
+    
+    return $filter;
+}
+
+
+1;
