4bbdbce1f75123d1b671ad834a528acc7787d1d9
Patrick Jendral <pj@itnode.de>
2016-06-01 17:16:54 +0200

    Empty package shells with methods

---
+++ lib/Webservice/OVH/Email/Domain/Domain/Account.pm
@@ -0,0 +1,77 @@
+package Webservice::OVH::Email::Domain::Domain::Account;
+
+use strict;
+use warnings;
+use Carp qw{ carp croak };
+
+our $VERSION = 0.1;
+
+sub _new_existing {
+    
+    
+}
+
+sub _new {
+    
+    
+    
+}
+
+sub properties {
+    
+    
+}
+
+sub is_blocked {
+    
+    
+}
+
+sub email {
+    
+    
+}
+
+sub domain {
+    
+    
+}
+
+sub description {
+    
+    
+}
+
+sub name {
+    
+    
+}
+
+sub size {
+    
+    
+}
+
+sub change {
+    
+    
+}
+
+sub delete {
+    
+    
+}
+
+# needs some string validation
+# password (length : [9;30], no space at begin and end, no accent)
+sub change_password {
+    
+    
+}
+
+sub usage {
+    
+    
+}
+
+1;
---
+++ lib/Webservice/OVH/Email/Domain/Domain/MailingList.pm
@@ -0,0 +1,124 @@
+package Webservice::OVH::Email::Domain::Domain::Account;
+
+use strict;
+use warnings;
+use Carp qw{ carp croak };
+
+our $VERSION = 0.1;
+
+sub _new_existing {
+    
+    
+}
+
+sub _new {
+    
+    
+}
+
+sub id {
+    
+    
+}
+
+sub name {
+    
+    
+}
+
+sub properties {
+    
+    
+}
+
+sub language {
+    
+    
+}
+
+sub options {
+    
+    
+}
+
+sub owner_email {
+    
+    
+}
+
+sub reply_to {
+    
+    
+}
+
+sub nb_subscribers_update_date {
+    
+    
+}
+
+sub nb_subscribers {
+    
+    
+}
+
+sub change {
+    
+    
+}
+
+sub delete {
+    
+    
+}
+
+sub change_options {
+    
+    
+}
+
+sub moderator {
+    
+    
+}
+
+sub add_moderator {
+    
+    
+}
+
+sub delete_moderator {
+    
+    
+}
+
+sub properties_moderator {
+    
+    
+}
+
+sub send_list_by_email {
+    
+    
+}
+
+sub subscriber {
+    
+    
+}
+
+sub add_subscriber {
+    
+    
+}
+
+sub properties_subscriber {
+    
+    
+}
+
+sub delete_subscriber {
+    
+    
+}
+
+1;
