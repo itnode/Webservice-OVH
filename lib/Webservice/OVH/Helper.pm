7db4431291e7f719e3b30d4df5a9f70c1e23f92f
Patrick Jendral <pj@itnode.de>
2016-06-01 10:41:32 +0200

    String to DateTime parser helper method

--- lib/Webservice/OVH/Helper.pm
+++ lib/Webservice/OVH/Helper.pm
@@ -1,30 +1,49 @@
 package Webservice::OVH::Helper;
 
+use strict;
+use warnings;
+use Carp qw{ carp croak };
+
+use DateTime::Format::Strptime;
+
 sub construct_filter {
-    
-    my ($class, %params) = @_;
-    
+
+    my ( $class, %params ) = @_;
+
     my @params = keys %params;
     my @values = values %params;
     my $filter = scalar @values ? '?' : "";
-    
-    foreach my $param ( @params ) {
-        
+
+    foreach my $param (@params) {
+
         my $value = $params{$param};
         next unless $value;
-        
+
         $value = $value eq '_empty_' ? "" : $value;
-        
-        if( $filter ne '?') {
-            
-            $filter.= '&';
+
+        if ( $filter ne '?' ) {
+
+            $filter .= '&';
         }
-        
-        $filter .= sprintf("%s=%s", $param, $value);
+
+        $filter .= sprintf( "%s=%s", $param, $value );
     }
-    
+
     return $filter;
 }
 
+sub parse_datetime {
+
+    my ( $class, $str_datetime, $locale, $timezone ) = @_;
+
+    my $strp = DateTime::Format::Strptime->new(
+        pattern   => '%FT%T',
+        locale    => ( $locale || 'de_DE' ),
+        time_zone => ( $timezone || 'Europe/Berlin' ),
+        on_error  => 'croak',
+    );
+
+    return $strp->parse_datetime($str_datetime);
+}
 
 1;
