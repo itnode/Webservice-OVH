aac40675aa5808d340c58810ce4e52f001efbc92
Jens Gassmann <jg@gassmann.it>
2021-06-10 12:22:06 +0200

    Checking in changes prior to tagging of version 0.43.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index af592e7..6ae5e23 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Webservice-OVH
     
     {{$NEXT}}
     
    +0.43 2021-06-10T10:21:46Z
    +
    +    - Add support for cards 
    +
     0.42 2018-11-22T09:35:38Z
     
         - Filter construction fixes

--- lib/Webservice/OVH.pm
+++ lib/Webservice/OVH.pm
@@ -44,7 +44,7 @@ use strict;
 use warnings;
 use Carp qw{ carp croak };
 
-our $VERSION = 0.42;
+our $VERSION = 0.43;
 
 # api module provided by ovh
 use OVH::OvhApi;
--- lib/Webservice/OVH/Order/Cart.pm
+++ lib/Webservice/OVH/Order/Cart.pm
@@ -30,7 +30,7 @@ use strict;
 use warnings;
 use Carp qw{ carp croak };
 
-our $VERSION = 0.42;
+our $VERSION = 0.43;
 
 use Webservice::OVH::Order::Cart::Item;
 
